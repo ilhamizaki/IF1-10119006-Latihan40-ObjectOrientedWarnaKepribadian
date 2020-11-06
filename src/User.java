e257c35eb703978d78b75f7bdb9079fd5e8de19a
ilhamizaki <72953685+ilhamizaki@users.noreply.github.com>
2020-11-06 17:38:23 +0700

    Add files via upload

---
+++ src/User.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+/**
+ *
+ * @author
+ * NAMA     : Ilham zaki
+ * Kelas    : IF 1
+ * NIM      : 10119006
+ * Deskripsi Program : class input user
+ */
+public class User {
+    Scanner scan = new Scanner(System.in);
+    private String namaUser, warnaUser;
+
+    public void masukkanNama(){
+        System.out.print("NAMA KAMU : ");
+        namaUser = scan.next();
+    }
+
+    public void masukkanWarna(){
+        Warna warna = new Warna();
+        String foreground = warna.foreground;
+        String background = warna.background;
+        String netral = warna.netral;
+        String merah = warna.merah;
+        String hijau = warna.hijau;
+        String kuning = warna.kuning;
+        String cyan = warna.cyan;
+        String ungu = warna.ungu;
+        String biru = warna.biru;
+        String putih = warna.putih;
+        Scanner scan = new Scanner(System.in);
+        System.out.println(foreground.concat(merah).concat("YUK ")
+                .concat(foreground).concat(hijau).concat("CEK ")
+                .concat(foreground).concat(kuning).concat("KEPRIBADIANMU ")
+                .concat(foreground).concat(cyan).concat("DARI ")
+                .concat(foreground).concat(ungu).concat("WARNA ")
+                .concat(foreground).concat(biru).concat("FAVORITMU\n"));
+
+        System.out.println(background.concat(merah).concat("\t\t").concat(foreground)
+                .concat(putih).concat("MERAH").concat("\t\t").concat(netral));
+        System.out.println(background.concat(hijau).concat("\t\t")
+                .concat(foreground).concat(putih).concat("HIJAU").concat("\t\t").concat(netral));
+        System.out.println(background.concat(kuning).concat("\t\t")
+                .concat(foreground).concat(putih).concat("KUNING").concat("\t\t").concat(netral));
+        System.out.println(background.concat(biru).concat("\t\t")
+                .concat(foreground).concat(putih).concat("BIRU").concat("\t\t").concat(netral));
+        System.out.println(background.concat(ungu).concat("\t\t")
+                .concat(foreground).concat(putih).concat("UNGU").concat("\t\t").concat(netral));
+
+        System.out.print("\nPILIH WARNA FAVORITMU : ");
+        warnaUser = scan.next().toUpperCase();
+    }
+
+    public void menentukanKepribadian(){
+        Kepribadian kepribadian = new Kepribadian();
+        kepribadian.mencariKepribadian(warnaUser, namaUser);
+    }
+}
---
+++ src/Warna.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * NAMA     : Ilham zaki
+ * Kelas    : IF 1
+ * NIM      : 10119006
+ * Deskripsi Program : class untuk Warna
+ */
+public class Warna {
+    public final String foreground = "\033[38;2;";
+    public final String background = "\033[48;2;";
+    public final String netral = "\033[0m";
+    public final String merah = "255;0;0m";
+    public final String hijau = "0;255;0m";
+    public final String biru = "0;0;255m";
+    public final String kuning = "255;255;0m";
+    public final String ungu = "255;0;255m";
+    public final String cyan = "0;255;255m";
+    public final String putih = "255;255;255m";
+}
